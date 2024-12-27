21a1e5c3dbe6882acb72d9712e92d48eb4a49332
Life Events <lifeevents001@gmail.com>
2024-12-27 10:17:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,89 +6,28 @@ const path = require("path")
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const profileRoutes = require('./routes/profile');
+const frontendapi = require('./routes/frontendapi.js');
+const TestimonialModel = require('./models/TestimonialSchema.js');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Middleware
 app.use(cors());
+app.use(express.json())
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "views", "dist")));
-
-// Define UserModel for testimonials
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    testimonial: { type: String, required: true },
-    date: { type: Date, default: Date.now }
+app.use(express.static(path.join(__dirname,'views',"dist")));
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store');
+  next();
 });
 
-const UserModel = mongoose.model('testimonial', userSchema);
-
-// Routes
-app.get("/testimonials", async (req, res) => {
-    try {
-      let data = await UserModel.find();
-      res.status(200).send(data);
-    } catch (error) {
-      res.status(401).send({ msg: error.message});
-    }
-  });
-  app.get("/users", async (req, res) => {
-    try {
-      let data = await UserModel.find();
-      res.status(200).send(data);
-    } catch (error) {
-      res.status(401).send({ msg: error.message});
-    }
-  });
-  app.get("/teams", async (req, res) => {
-    try {
-      let data = await UserModel.find();
-      res.status(200).send(data);
-    } catch (error) {
-      res.status(401).send({ msg: error.message});
-    }
-  });
-
-  app.get("/slide", async (req, res) => {
-    try {
-      let data = await UserModel.find();
-      res.status(200).send(data);
-    } catch (error) {
-      res.status(401).send({ msg: error.message});
-    }
-  });
-  app.get("/gallery", async (req, res) => {
-    try {
-      let data = await UserModel.find();
-      res.status(200).send(data);
-    } catch (error) {
-      res.status(401).send({ msg: error.message});
-    }
-  });
-  app.get("/slider", async (req, res) => {
-    try {
-      let data = await UserModel.find();
-      res.status(200).send(data);
-    } catch (error) {
-      res.status(401).send({ msg: error.message});
-    }
-  });
-  app.get("/services", async (req, res) => {
-    try {
-      let data = await UserModel.find();
-      res.status(200).send(data);
-    } catch (error) {
-      res.status(401).send({ msg: error.message});
-    }
-  });
-
-app.get("/",(req,res)=>{
-  res.sendFile(path.join(__dirname, "views", "dist", "index.html"));
-})
+app.use('/', frontendapi);
 app.use('/api/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
-
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'views','dist','index.html'))
+})
 // MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -96,5 +35,5 @@ mongoose
   .catch((err) => console.error('MongoDB connection error:', err));
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
