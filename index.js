e121efea5ef69e8549e92d8135732675d53261db
Life Events <lifeevents001@gmail.com>
2024-12-28 12:19:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json())
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname,'views',"dist")));
 app.use((req, res, next) => {
   res.set('Cache-Control', 'no-store');
   next();
@@ -25,6 +24,8 @@ app.use((req, res, next) => {
 app.use('/', frontendapi);
 app.use('/api/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
+app.use(express.static(path.join(__dirname,'views',"dist")));
+
 app.get('*',(req,res)=>{
   res.sendFile(path.join(__dirname,'views','dist','index.html'))
 })
@@ -35,5 +36,5 @@ mongoose
   .catch((err) => console.error('MongoDB connection error:', err));
 
 // Start the server
-const PORT = process.env.PORT ;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT;
+app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port ${PORT}`));
