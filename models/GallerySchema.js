21b5adc020f6153761366ff58be243cc761d6992
Life Events <lifeevents001@gmail.com>
2025-02-16 23:40:31 +0530

    Delete models directory

--- models/GallerySchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-
-const GallerySchema = mongoose.Schema({
-    image:{type:String},
-});
-
-const GalleryModel=mongoose.model('galleries',GallerySchema)
-
-module.exports=GalleryModel
--- models/SliderSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-
-const SliderSchema = mongoose.Schema({
-    image:{type:String},
-});
-
-const sliderModel=mongoose.model('slider',SliderSchema)
-
-module.exports=sliderModel
